c91fa5ee12c59f7a7f6aff3cb82b48c07b7d4c60
Joe <doode1524@gmail.com>
2022-02-05 23:16:20 -0500

    user seed init

--- db/migrate/20220206034931_create_users.rb
+++ db/migrate/20220206034931_create_users.rb
@@ -3,7 +3,11 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-
+      t.integer :current_streak
+      t.integer :best_streak
+      t.integer :wins
+      t.integer :losses
+      
       t.timestamps
     end
   end
